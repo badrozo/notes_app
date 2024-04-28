db206b41859018763bfe6114df99bd4fbb92e36e
badrozo <ffbadroff@gmail.com>
2024-04-28 13:43:41 +0100

    Adding CustomAppBar NotesView NotesViewBody

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_view.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 
 void main() {
   runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
-  const NotesApp({super.key});
+  const NotesApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Scaffold(),
+      home: Scaffold(
+        body: const NotesViewBody(),
+      ),
     );
   }
 }
