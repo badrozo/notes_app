0872579e0ec75394173bb0c62a6c755ccdcc9cf1
badrozo <ffbadroff@gmail.com>
2024-05-01 15:18:20 +0100

    adding EditNotView

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,46 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(bottom: 24, top: 24, left: 24),
-        decoration: BoxDecoration(
-          color: Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          ListTile(
-            title: Text(
-              'Note 1',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Note 1 description',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(.6), fontSize: 18),
-              ),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 22,
-                )),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditNoteView();
+        }));
+      },
+      child: Container(
+          padding: EdgeInsets.only(bottom: 24, top: 24, left: 24),
+          decoration: BoxDecoration(
+            color: Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              '04/03/2023',
-              style: TextStyle(color: Colors.black.withOpacity(.4)),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+            ListTile(
+              title: Text(
+                'Note 1',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Note 1 description',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.6), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 22,
+                  )),
             ),
-          )
-        ]));
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                '04/03/2023',
+                style: TextStyle(color: Colors.black.withOpacity(.4)),
+              ),
+            )
+          ])),
+    );
   }
 }
